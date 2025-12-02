22f0e4253134aa46977f06c30ccda49b78d5807f
keerthi7612 <keerthanakumari2003@gmail.com>
2025-12-02 11:58:25 +0530

    Added getAllItems

--- controller/itemController.js
+++ controller/itemController.js
@@ -1,6 +1,8 @@
 import Item from "../model/itemModel.js";
 import { errorResponse } from "../utils/responseUtil.js";
 
+//####################post####################################
+
 export const createItem = async (req, res) => {
   try {
     const { title, description } = req.body;
@@ -48,6 +50,91 @@ export const createItem = async (req, res) => {
       errorResponse("Internal server error", {
         type: "server",
         msg: "Unexpected error occurred",
+        path: null,
+        location: null,
+      })
+    );
+  }
+};
+
+//###################GET##############################
+
+export const getAllItems = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const { sort = "asc", search = "", page = 1, limit = 5 } = req.query;
+
+    if (!["asc", "desc"].includes(sort.toLowerCase())) {
+      return res.status(400).json(
+        errorResponse("Validation failed", {
+          type: "query",
+          msg: "Invalid sort value",
+          path: "sort",
+          location: "query",
+        })
+      );
+    }
+
+    if (isNaN(page) || page <= 0) {
+      return res.status(400).json(
+        errorResponse("Validation failed", {
+          type: "query",
+          msg: "Invalid page value",
+          path: "page",
+          location: "query",
+        })
+      );
+    }
+
+    const searchFilter =
+      search.trim() !== "" ? { title: { $regex: search, $options: "i" } } : {};
+
+    const skip = (page - 1) * limit;
+
+    const items = await Item.find({
+      userId,
+      ...searchFilter,
+    })
+      .sort({ createdAt: sort === "asc" ? 1 : -1 })
+      .skip(skip)
+      .limit(limit);
+
+    if (items.length === 0) {
+      return res.status(404).json(
+        errorResponse("Item not found", {
+          type: "not_found",
+          msg: "No items found for this user",
+          path: "items",
+          location: "database",
+        })
+      );
+    }
+
+    const totalItems = await Item.countDocuments({ userId, ...searchFilter });
+    const totalPages = Math.ceil(totalItems / limit);
+
+    return res.status(200).json({
+      success: true,
+      message: "Items fetched successfully",
+      items: items.map((item) => ({
+        id: item._id,
+        title: item.title,
+        description: item.description,
+      })),
+      pagination: {
+        currentPage: Number(page),
+        totalPages,
+        totalItems,
+      },
+    });
+  } catch (error) {
+    console.log("GET ITEMS ERROR:", error);
+
+    return res.status(500).json(
+      errorResponse("Internal server error", {
+        type: "server",
+        msg: "Unexpected server error",
         path: null,
         location: null,
       })
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,10 +1,11 @@
 import Router from "express";
-import { createItem } from "../controller/itemController.js";
+import { createItem, getAllItems } from "../controller/itemController.js";
 import { itemMiddleware } from "../middleware/itemMiddleware.js";
 import { validateCreateItem } from "../validation/itemValidator.js";
 
 const itemRoutes = Router();
 
+itemRoutes.get("/getItems", itemMiddleware, getAllItems);
 itemRoutes.post("/create", itemMiddleware, createItem, validateCreateItem);
 
 export default itemRoutes;
